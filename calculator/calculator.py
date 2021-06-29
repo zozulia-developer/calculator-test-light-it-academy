681527065d1f50f0fa62959142c774f60527d62f
kirill <035386@iskra.local>
2021-06-29 08:49:47 +0300

    tests impl

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -2,36 +2,46 @@
 from math import sqrt
 
 
+def is_int(x, y=1):
+    """ Check if value is integer """
+    if not isinstance(x, int) or not isinstance(y, int):
+        raise TypeError(f"Values must be an integer!")
+
+
 class Calculator:
     """ Calculator implementation """
+
     def add(self, x: int, y: int) -> int:
         """ Add to attributes to each other """
+        is_int(x, y)
         return x + y
 
     def subtract(self, x: int, y: int) -> int:
         """ Subtract one attribute from another """
+        is_int(x, y)
         return x - y
 
     def divide(self, x: int, y: int) -> float:
         """ Divide x attribute on y """
-        return x / y
+        is_int(x, y)
+        return x / y if y != 0 else ZeroDivisionError
 
     def multiply(self, x: int, y: int) -> int:
         """ Multiply x attribute on y """
+        is_int(x, y)
         return x * y
 
     def mod(self, x: int, y: int) -> int:
         """ Take mod of one attribute from another """
-        return x % y
+        is_int(x, y)
+        return x % y if y != 0 else ZeroDivisionError
 
     def power(self, x: int, y: int) -> int:
         """ Raise attributes x to a power y """
+        is_int(x, y)
         return x ** y
 
     def root(self, x: int) -> float:
         """ Take a root from attributes """
+        is_int(x)
         return sqrt(x)
-
-
-c = Calculator()
-print(c.add(2, 3))
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -8,5 +8,120 @@ def calc() -> Calculator:
     return Calculator()
 
 
-def test_add(calc: Calculator):
-    assert calc.add(1, 1) == 1 + 1
+@pytest.mark.parametrize(
+    'first_value, second_value, result',
+    [
+        (1, 1, 2),
+        (-1, 3, 2),
+        (3, -1, 2),
+        (0, 0, 0),
+    ]
+)
+def test_add(
+        calc: Calculator,
+        first_value: int,
+        second_value: int,
+        result: int):
+    assert calc.add(first_value, second_value) == result
+
+
+@pytest.mark.parametrize(
+    'first_value, second_value, result',
+    [
+        (1, 1, 0),
+        (-1, 3, -4),
+        (3, -1, 4),
+        (0, 0, 0)
+    ]
+)
+def test_subtract(
+        calc: Calculator,
+        first_value: int,
+        second_value: int,
+        result: int):
+    assert calc.subtract(first_value, second_value) == result
+
+
+@pytest.mark.parametrize(
+    'first_value, second_value, result',
+    [
+        (2, 2, 1),
+        (10, 2, 5),
+        (6, -3, -2),
+        (-3, 6, -0.5),
+        (2, 0, ZeroDivisionError),
+        (0, 2, 0)
+    ]
+)
+def test_divide(
+        calc: Calculator,
+        first_value: int,
+        second_value: int,
+        result: int):
+    assert calc.divide(first_value, second_value) == result
+
+
+@pytest.mark.parametrize(
+    'first_value, second_value, result',
+    [
+        (1, 1, 1),
+        (-1, 3, -3),
+        (3, -2, -6),
+        (0, 0, 0),
+        (2, 0, 0)
+    ]
+)
+def test_multiply(
+        calc: Calculator,
+        first_value: int,
+        second_value: int,
+        result: int):
+    assert calc.multiply(first_value, second_value) == result
+
+
+@pytest.mark.parametrize(
+    'first_value, second_value, result',
+    [
+        (1, 1, 0),
+        (-1, 3, 2),
+        (3, -2, -1),
+        (2, 2, 0),
+        (2, 0, ZeroDivisionError),
+    ]
+)
+def test_mod(
+        calc: Calculator,
+        first_value: int,
+        second_value: int,
+        result: int):
+    assert calc.mod(first_value, second_value) == result
+
+
+@pytest.mark.parametrize(
+    'first_value, second_value, result',
+    [
+        (1, 1, 1),
+        (-1, 3, -1),
+        (3, 3, 27)
+    ]
+)
+def test_power(
+        calc: Calculator,
+        first_value: int,
+        second_value: int,
+        result: int):
+    assert calc.power(first_value, second_value) == result
+
+
+@pytest.mark.parametrize(
+    'first_value, result',
+    [
+        (9, 3),
+        (25, 5),
+    ]
+)
+def test_root(
+        calc: Calculator,
+        first_value: int,
+        result: float):
+    assert calc.root(first_value) == result
